263e5369f0488fcde936a479081f8afe31f56405
gandepallipujitha <136604837+gandepallipujitha@users.noreply.github.com>
2023-08-09 17:47:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,113 @@
+#this file consists of code for instances and sg
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIA46AYPMRJI5FRFE7B"
+secret_key = "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"
+}
 
+resource "aws_instance" "one" {
+  ami             = "ami-09538990a0c4fe9be"
+  instance_type   = "t2.micro"
+  key_name        = "virginanorth"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "us-east-1a"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my app created by terraform infrastructurte by pujitha sir server-1" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-1"
+  }
+}
+
+resource "aws_instance" "two" {
+  ami             = "ami-09538990a0c4fe9be"
+  instance_type   = "t2.micro"
+  key_name        = "virginanorth"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "us-east-1b"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by pujitha sir server-2" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-2"
+  }
+}
+
+resource "aws_instance" "three" {
+  ami             = "ami-09538990a0c4fe9be"
+  instance_type   = "t2.micro"
+  key_name        = "virginanorth"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "app-server-1"
+  }
+}
+
+resource "aws_instance" "four" {
+  ami             = ""ami-09538990a0c4fe9be
+  instance_type   = "t2.micro"
+  key_name        = "virginanorth"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "app-server-2"
+  }
+}
+
+resource "aws_security_group" "five" {
+  name = "elb-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "six" {
+  bucket = "pujitha223199"
+}
+
+resource "aws_iam_user" "seven" {
+for_each = var.user_names
+name = each.value
+}
+
+variable "user_names" {
+description = "*"
+type = set(string)
+default = ["user1", "user2", "user3", "user4"]
+}
+
+resource "aws_ebs_volume" "eight" {
+ availability_zone = "us-east-1a"
+  size = 40
+  tags = {
+    Name = "ebs-001"
+  }
+}
